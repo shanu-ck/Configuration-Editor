41512b741946d9481855950087ecd39389e6ce94
shanu-ck <shanu.dey@cloudkaptan.com>
2022-12-13 17:29:09 +0530

    implement header
    Signed-off-by: shanu-ck <shanu.dey@cloudkaptan.com>

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function Header() {
+    return (
+        <>
+            <Navbar bg="light">
+                <Container>
+                    <Navbar.Brand href="/">Configuration Editor</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default Header;

6d2bf3e5fc6cca098eb59b42e48613c686948528
shanu-ck <shanu.dey@cloudkaptan.com>
2022-12-13 17:30:25 +0530

    Implement form component as body
    Signed-off-by: shanu-ck <shanu.dey@cloudkaptan.com>

---
+++ src/components/FormComponent.js
@@ -0,0 +1,94 @@
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import TreeComponent from "./TreeComponent";
+import JsonDataContext from "../contexts/JsonDataContext";
+
+export default function FormComponent() {
+    const [jsonData, setJsonData] = useState();
+    const [filename, setFilename] = useState("filename");
+
+    const updateConfigFile = () => {
+        console.log("Updated json", jsonData);
+    };
+
+    const generateDownloadableFile = () =>
+        `data:text/json;charset=utf-8,${encodeURIComponent(
+            JSON.stringify(jsonData, null, 2)
+        )}`;
+
+    const showFile = async (e) => {
+        e.preventDefault();
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const text = e.target.result;
+            setJsonData(JSON.parse(text));
+        };
+        reader.readAsText(e.target.files[0]);
+        setFilename(e.target.files[0].name);
+    };
+
+    const downloadFile = ({ data, fileName, fileType }) => {
+        // Create a blob with the data we want to download as a file
+        const blob = new Blob([data], { type: fileType });
+        // Create an anchor element and dispatch a click event on it
+        // to trigger a download
+        const a = document.createElement("a");
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+    };
+
+    const exportToJson = (e) => {
+        e.preventDefault();
+        downloadFile({
+            data: JSON.stringify(jsonData),
+            fileName: filename,
+            fileType: "text/json",
+        });
+    };
+
+    return (
+        <JsonDataContext.Provider value={{ jsonData, setJsonData }}>
+            <Container style={{ marginTop: 20 }}>
+                <Card>
+                    <Card.Header className="text-center">
+                        Configuration Editor
+                    </Card.Header>
+                    <Card.Body>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Select configuration file</Form.Label>
+                            <Form.Control
+                                type="file"
+                                onChange={(e) => showFile(e)}
+                            />
+                        </Form.Group>
+                        {jsonData && (
+                            <>
+                                <Card.Title>File Content</Card.Title>
+                                <TreeComponent />
+                                <Button
+                                    variant="primary"
+                                    onClick={exportToJson}
+                                >
+                                    Download
+                                </Button>
+                            </>
+                        )}
+                    </Card.Body>
+                    <Card.Footer className="text-muted">
+                        Developed by @Shanu-CK
+                    </Card.Footer>
+                </Card>
+            </Container>
+        </JsonDataContext.Provider>
+    );
+}

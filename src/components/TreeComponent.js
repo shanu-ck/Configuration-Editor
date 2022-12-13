68d9e77104d168baa53c482a404d40c91ee76c61
shanu-ck <shanu.dey@cloudkaptan.com>
2022-12-13 17:30:48 +0530

    implemented recursive tree component with css
    Signed-off-by: shanu-ck <shanu.dey@cloudkaptan.com>

---
+++ src/components/TreeComponent.js
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import NestedComponent from "./NestedComponent";
+import "./TreeComponent.css";
+import JsonDataContext from "../contexts/JsonDataContext";
+
+const TreeComponent = () => {
+    const {jsonData }= useContext(JsonDataContext);
+    const keyGen = (keyPath, key) => [...keyPath, key];
+
+    const processObject = (object, keyPath) =>
+        Object.keys(object).map((key, reactKey) => {
+            let newKeyPath = keyGen(keyPath, key);
+            return (
+                <li key={reactKey + key}>
+                    {buildNode(key)}
+                    <ul className="nested">
+                        {isPrimative(object[key])
+                            ? buildLeaf(object[key], newKeyPath)
+                            : isArray(object[key])
+                            ? loopArray(object[key], newKeyPath)
+                            : processObject(object[key], newKeyPath)}
+                    </ul>
+                </li>
+            );
+        });
+
+    const loopArray = (array, keyPath) =>
+        array.map((value, key) => {
+            let newKeyPath = keyGen(keyPath, key);
+            return (
+                <div key={key + value}>
+                    {isPrimative(value)
+                        ? buildLeaf(value, newKeyPath)
+                        : isArray(value)
+                        ? loopArray(value, newKeyPath)
+                        : processObject(value, newKeyPath)}
+                </div>
+            );
+        });
+
+    const isArray = (value) => Array.isArray(value);
+
+    const isPrimative = (value) => {
+        return (
+            typeof value === "string" ||
+            typeof value === "number" ||
+            typeof value === "boolean"
+        );
+    };
+
+    const buildNode = (key) => (
+        <span
+            className="node"
+            onClick={(e) => {
+                toggle(e);
+            }}
+        >
+            {key}
+        </span>
+    );
+
+    const buildLeaf = (value, keyPath) => (
+        <li className="leaf" onClick={(e) => {}}>
+            <NestedComponent keyPath={keyPath}/>
+        </li>
+    );
+
+    const toggle = (event) => {
+        event.target.parentElement
+            .querySelector(".nested")
+            .classList.toggle("active");
+        event.target.classList.toggle("node-down");
+    };
+
+    return (
+        <>
+            <ul id="myUL">{processObject(jsonData, [])}</ul>
+        </>
+    );
+};
+
+export default TreeComponent;
